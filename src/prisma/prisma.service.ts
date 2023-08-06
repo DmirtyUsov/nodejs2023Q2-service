89afbd2b518d0513551bfd73d4506a89d97f0b59
Dima Usov <dmitry.usov@yahoo.com>
2023-08-06 21:24:59 +0300

    feat: add type converter for prisma  User queries

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+// extension to transform fields type in results returned by Prisma queries
+const createPrismaExtended = (prisma: PrismaClient) =>
+  prisma.$extends({
+    result: {
+      user: {
+        // DateTime to number
+        createdAt: {
+          needs: { createdAt: true },
+          compute(user) {
+            return user.createdAt.getTime();
+          },
+        },
+        // DateTime to number
+        updatedAt: {
+          needs: { updatedAt: true },
+          compute(user) {
+            return user.updatedAt.getTime();
+          },
+        },
+      },
+    },
+  });
+
 @Injectable()
 export class PrismaService extends PrismaClient {
+  private _prisma: ReturnType<typeof createPrismaExtended>;
   constructor() {
     super({
       datasources: {
         db: {
-          url: 'postgresql://hluser:123@localhost:5432/hldb?schema=public',
+          url: 'postgresql://hluser:123@hl-database:5432/hldb?schema=public',
         },
       },
     });
+  }
+
+  // Create an Extended instance when needed
+  get extended() {
+    if (!this._prisma) {
+      this._prisma = createPrismaExtended(this);
+    }
+
+    return this._prisma;
   }
 }
