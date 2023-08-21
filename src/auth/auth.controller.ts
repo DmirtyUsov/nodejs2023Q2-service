9993a0c286419e62ac0db58310cb3a44254357ea
Dima Usov <dmitry.usov@yahoo.com>
2023-08-21 19:40:29 +0300

    feat: add routes auth/login, auth signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CredentialUserDto } from 'src/user/dto';
 import { AuthDto } from './dto';
+import { StatusCodes } from 'http-status-codes';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('signup')
-  async signup(@Body() dto: CredentialUserDto): Promise<AuthDto> {
-    return this.authService.signup(dto);
-  }
-
   @Post('login')
-  async login() {
-    return this.authService.login();
+  async login(@Body() dto: CredentialUserDto): Promise<AuthDto> {
+    return await this.authService.login(dto);
   }
 
+  @HttpCode(StatusCodes.NO_CONTENT)
+  @Post('signup')
+  async signup(@Body() dto: CredentialUserDto): Promise<void> {
+    await this.authService.signup(dto);
+  }
   @Post('refresh')
   async refresh() {
     return this.authService.refresh();
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { CredentialUserDto } from 'src/user/dto';
 import { AuthDto } from './dto';
 import { UserService } from 'src/user/user.service';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
 
-  async signup(dto: CredentialUserDto): Promise<AuthDto> {
-    const result = await this.userService.createUser(dto);
-    return { token: 'Your Token' };
+  async signup(dto: CredentialUserDto): Promise<void> {
+    const loginInDb = await this.userService.getUserByLogin(dto.login);
+    if (loginInDb) {
+      throw new ConflictException('Login exists');
+    }
+    await this.userService.createUser(dto);
   }
 
-  async login() {
-    return 'login';
+  async login(dto: CredentialUserDto): Promise<AuthDto> {
+    const result = await this.userService.getUserByLogin(dto.login);
+    if (!result) {
+      throw new ForbiddenException('No user');
+    }
+    const isHashesSame = await compare(dto.password, result.password);
+    if (!isHashesSame) {
+      throw new ForbiddenException('Wrong password');
+    }
+    return { token: result.id };
   }
 
   async refresh() {
