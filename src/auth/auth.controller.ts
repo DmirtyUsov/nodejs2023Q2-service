da25f790dc0c81e96200659027ae91ac2bab7bba
Dima Usov <dmitry.usov@yahoo.com>
2023-08-21 22:25:17 +0300

    feat: add auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,19 @@ import { AuthService } from './auth.service';
 import { CredentialUserDto } from 'src/user/dto';
 import { AuthDto } from './dto';
 import { StatusCodes } from 'http-status-codes';
+import { Public } from './decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('login')
   async login(@Body() dto: CredentialUserDto): Promise<AuthDto> {
     return await this.authService.login(dto);
   }
 
+  @Public()
   @HttpCode(StatusCodes.NO_CONTENT)
   @Post('signup')
   async signup(@Body() dto: CredentialUserDto): Promise<void> {
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [JwtModule.register({})],
-  providers: [AuthService, UserService],
+  providers: [
+    AuthService,
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
